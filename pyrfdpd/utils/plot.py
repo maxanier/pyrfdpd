dc22697abbc9ae6c69a29ba9acdbb9e01122e33b
Zhe Li <ataraxialex@gmail.com>
2024-05-30 20:27:09 +0800

    sync

--- pyrfdpd/utils/plot.py
+++ pyrfdpd/utils/plot.py
@@ -1,5 +1,3 @@
-from cProfile import label
-from tkinter import font
 from .metrics import rms
 import matplotlib.pyplot as plt
 import numpy as np
@@ -14,46 +12,59 @@ def psd(signals: dict, fs: float = 122.88e6, filename: str = "psd.png", lang="en
         plt.xlabel("Frequency Offset (Hz)")
         plt.ylabel("Normalized Power Spectral Density (dB/Hz)")
     elif (lang == "zh"):
-        plt.xlabel("频偏 (Hz)", fontproperties='SimHei')
-        plt.ylabel("归一化功率谱密度 (dB/Hz)", fontproperties='SimHei')
+        plt.xlabel("频偏 (Hz)")
+        plt.ylabel("归一化功率谱密度 (dB/Hz)")
     else:
         raise ValueError("Language not supported")
-    plt.legend(loc="upper right", fontsize=15)
+    plt.legend(loc="upper right", fontsize=18)
     plt.savefig(filename)
     plt.close()
 
 
-def amam(x, y: dict, filename="amam.png", lang="en"):
+def amam(x, y, filename="amam.png", lang="en"):
     x = x / max(abs(x))
-    for name, signal in y.items():
-        signal = signal / max(abs(signal))
-        plt.scatter(abs(x), abs(signal), marker=".", s = 10, label=name)
+    if isinstance(y, dict):
+        for name, signal in y.items():
+            signal = signal / max(abs(signal))
+            plt.scatter(abs(x), abs(signal), marker=".", s = 10, label=name)
+        plt.legend(loc="upper left")
+    if isinstance(y, list):
+        for signal in y:
+            signal = signal / max(abs(signal))
+            plt.scatter(abs(x), abs(signal), marker=".", s = 10)
     if (lang == "en"):
         plt.xlabel("Normalized Input Amplitude")
         plt.ylabel("Normalized Output Amplitude")
     elif (lang == "zh"):
-        plt.xlabel("归一化输入幅度", fontproperties='SimHei')
-        plt.ylabel("归一化输出幅度", fontproperties='SimHei')
+        plt.xlabel("归一化输入幅度")
+        plt.ylabel("归一化输出幅度")
     else:
         raise ValueError("Language not supported")
-    plt.legend(loc = "upper left")
+    plt.grid(True, linestyle='--')
     plt.savefig(filename)
     plt.close()
 
 
-def ampm(x, y: dict, filename="ampm.png", lang="en"):
+def ampm(x, y, filename="ampm.png", lang="en"):
     x = x / max(abs(x))
-    for name, signal in y.items():
-        signal = signal / max(abs(signal))
-        plt.scatter(abs(x), np.angle(x / signal, deg=True), marker=".", s = 10, label=name)
+    if isinstance(y, dict):
+        for name, signal in y.items():
+            signal = signal / max(abs(signal))
+            plt.scatter(abs(x), np.angle(x / signal, deg=True), marker=".", s = 10, label=name)
+        plt.legend(loc="upper left")
+    if isinstance(y, list):
+        for signal in y:
+            signal = signal / max(abs(signal))
+            plt.scatter(abs(x), np.angle(x / signal, deg=True), marker=".", s = 10)
     if (lang == "en"):
         plt.xlabel("Normalized Input Amplitude")
         plt.ylabel("Phase Difference (degree)")
     elif (lang == "zh"):
-        plt.xlabel("归一化输入幅度", fontproperties='SimHei')
-        plt.ylabel("相位差 (度)", fontproperties='SimHei')
+        plt.xlabel("归一化输入幅度")
+        plt.ylabel("相位差 (度)")
     else:
         raise ValueError("Language not supported")
+    plt.grid(True, linestyle='--')
     plt.legend(loc="upper right")
     plt.savefig(filename)
     plt.close()
